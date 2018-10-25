9e7846f96a6c1b67e5d55683fc43bd3196f14e63
Prakash <prakashdas1998.pd@gmail.com>
2018-10-25 16:24:12 +0530

    Initialized app

---
+++ src/header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component{
+    
+
+    render(){
+
+        return(
+            <div className="Header">
+            
+            </div>
+        ) 
+    }
+    
+}
+export default Header;
